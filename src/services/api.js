c322cb98bfc95e4904a69e5ed9bbaa8c6f1a4819
VicMariano <marianovictoria.11@gmail.com>
2021-12-30 03:56:30 -0300

    Modify: Previous endpoints, new ones added

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,22 @@
 import axios from "axios";
 
-export const getAllCards = async () => {
-  return axios.get(`http://localhost:3001/cards`);
+export const getAllWhiteCards = async () => {
+  return axios.get(`http://localhost:3001/whiteCards`);
 };
 
+export const getAllBlackCards = async () => {
+  return axios.get(`http://localhost:3001/blackCards`);
+};
+
+export const getMainBlackCard = async (id) => {
+  return axios.get(`http://localhost:3001/blackCards/` + id);
+};
+
+// endpoint para traer varias cartas blancas random, revisar all promises
+
 export const addCard = async ({ text, cardType }) => {
-  return axios.post(`http://localhost:3001/cards`, { text, cardType });
+  return axios.post(`http://localhost:3001/whiteCards`, {
+    text,
+    cardType,
+  });
 };
