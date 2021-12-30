0f8c612193ea64d237cc13d7f6f73fe0d06e2673
VicMariano <marianovictoria.11@gmail.com>
2021-12-30 04:03:26 -0300

    Modify: AddCard Component

--- src/components/AddCard/AddCard.js
+++ src/components/AddCard/AddCard.js
@@ -4,44 +4,62 @@ import { addCard } from "../../services/api";
 
 export default function AddCard() {
   const [form, setForm] = useState({ text: "", cardType: "white" });
+  const [submitSucces, setSubmitSucces] = useState(null);
   const handleChange = (e) => {
     setForm({ ...form, text: e.target.value });
   };
 
   const submitCard = async (e) => {
     e.preventDefault();
-    const response = await addCard({
-      text: form.text,
-      cardType: "white",
-    });
-    console.log("console log de linea 18", response);
+    try {
+      const postResponse = await addCard({
+        text: form.text,
+        cardType: "white",
+      });
+      setSubmitSucces(true);
+      console.log("Card Added Correctly", postResponse);
+    } catch (err) {
+      setSubmitSucces(false);
+      console.log(Object.keys(err), err.message, err.response);
+      throw new Error("Unable to get a token. ", err);
+    }
   };
+
+  useEffect(() => {
+    submitSucces && setForm({ text: "", cardType: "white" });
+  }, [submitSucces]);
 
   return (
     <div className="container">
-      <form>
-        <div className="card-creator">
-          <label className="label" htmlFor="cardText">
-            Texto de la carta:
-          </label>
-          <textarea
-            name="cardText"
-            id="text"
-            value={form.text}
-            onChange={handleChange}
-          />
-        </div>
-        {/* <input
-          type="text"
-          name="cardText"
-          id="text"
-          value={form.text}
-          onChange={handleChange}
-        /> */}
-        <button className="button" type="submit" onClick={submitCard}>
-          <strong className="plus">+</strong> Agregar carta
-        </button>
-      </form>
+      {submitSucces ? (
+        <h2>La carta fue agregada a la colección!</h2>
+      ) : (
+        <form className="container">
+          <div className="card-creator">
+            <label className="label" htmlFor="cardText">
+              Texto de la carta:
+            </label>
+            <textarea
+              name="cardText"
+              id="text"
+              value={form.text}
+              onChange={handleChange}
+            />
+          </div>
+
+          {submitSucces === false && (
+            <h3>Ups! Ocurrió un error al agregar la carta!</h3>
+          )}
+          <button
+            className="button"
+            disabled={form.text === ""}
+            type="submit"
+            onClick={submitCard}
+          >
+            <strong className="plus">+</strong> Agregar carta
+          </button>
+        </form>
+      )}
     </div>
   );
 }
