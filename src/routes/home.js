df5143b11b24d93650ca3580e58932dba25c4582
VicMariano <marianovictoria.11@gmail.com>
2022-01-21 11:58:16 -0300

    Create: Createroom Component

--- src/routes/home.js
+++ src/routes/home.js
@@ -12,80 +12,10 @@ import {
 import { getAuth } from "@firebase/auth";
 import firebaseApp from "../services/firebaseCredentials";
 import { useAuth } from "contexts/AuthContext";
+import { CreateRoom } from "components/CreateRoom/CreateRoom";
 const auth = getAuth(firebaseApp);
-const firestore = getFirestore(firebaseApp);
 
 export default function Home() {
-  const rooms = collection(firestore, "rooms");
-  const { user } = useAuth();
-  const userEmail = user.userEmail;
-
-  const findRoom = async (roomId) => {
-    // crear referencia
-    const docRef = doc(rooms, roomId);
-    // buscar doc
-    const consulta = await getDoc(docRef);
-
-    // revisar si existe
-    if (consulta.exists()) {
-      const infoDoc = consulta.data();
-      addUserToRoom(roomId, infoDoc.players);
-      // si si existe
-      console.log(infoDoc);
-      return;
-    } else {
-      alert("La sala a la que tratas de unirte no existe");
-      return;
-    }
-    // si no existe
-  };
-  const addUserToRoom = async (roomId, players) => {
-    console.log(players);
-    const docRef = doc(rooms, roomId);
-    const room = { players: [...players, userEmail] };
-    await updateDoc(docRef, room);
-    const consulta = await getDoc(docRef);
-    if (consulta.exists()) {
-      const data = consulta.data();
-      console.log(data);
-      return data;
-    } else {
-      console.log("No existe!!");
-    }
-  };
-  const createRoom = async () => {
-    const roomId = prompt("¿Cómo se va a lamar tu sala?: ");
-    // crear referencia
-    const docRef = doc(rooms, roomId);
-    // buscar doc
-    const consulta = await getDoc(docRef);
-    // revisar si existe
-    if (consulta.exists()) {
-      alert(
-        "El nombre de sala que ingresaste ya existe. Intenta de nuevo con un nombre diferente"
-      );
-      // si si existe
-      const infoDoc = consulta.data();
-      console.log(infoDoc);
-      return;
-    } else {
-      // si no existe
-      const newRoom = setDoc(docRef, { players: [userEmail] });
-      if (newRoom) {
-        alert("Sala Creada!");
-        console.log(newRoom);
-      } else {
-        alert("Error al crear la sala");
-        console.log();
-      }
-    }
-  };
-  const enterRommId = async () => {
-    const roomId = prompt("Ingresa el id de la sala: ");
-    if (roomId) {
-      findRoom(roomId);
-    }
-  };
   return (
     <main style={{ padding: "1rem 0" }}>
       <h1 style={{ padding: "1rem 0 2rem 0" }}>
@@ -94,16 +24,8 @@ export default function Home() {
       <h3 style={{ padding: "1rem 0 2rem 0" }}>
         Bienvenide {auth.currentUser.displayName} !
       </h3>
-      <div style={{ padding: "1rem 0 2rem 0" }}>
-        <ButtonComponent
-          text="Crear sala"
-          onClick={createRoom}
-        ></ButtonComponent>
-        <ButtonComponent
-          text="Entrar a una sala"
-          onClick={enterRommId}
-        ></ButtonComponent>
-      </div>
+
+      <CreateRoom></CreateRoom>
 
       <img className="home-img" src={image} />
     </main>
--- src/services/firestoreService.js
+++ src/services/firestoreService.js
@@ -12,12 +12,16 @@ import {
   where,
 } from "firebase/firestore";
 
+// CARDS
 const firestore = getFirestore(firebaseApp);
 const blackCardsRef = collection(firestore, "black-cards");
 const whiteCardsRef = collection(firestore, "white-cards");
+// USERS
 const usersRef = collection(firestore, "users");
-export let blackCards = [];
-export let whiteCards = [];
+// ROOMS
+const rooms = collection(firestore, "rooms");
+
+// CARDS
 
 // get all black cards
 export const getAllBlackCards = async () => {
@@ -78,6 +82,8 @@ export const addWhiteCard = async (text) => {
   }
 };
 
+// USERS
+
 export const addNewUser = async (user, roomId) => {
   const docRef = doc(usersRef, user.email);
   const newUser = {
@@ -97,6 +103,83 @@ export const addNewUser = async (user, roomId) => {
     console.error("No se pudo agregar el usuario");
   }
 };
+
+// ROOMS
+
+const createRoom = async (userEmail, roomId) => {
+  // crear referencia
+  const docRef = doc(rooms, roomId);
+  // buscar doc
+  const consulta = await getDoc(docRef);
+  // revisar si existe
+  if (consulta.exists()) {
+    // si si existe
+    alert(
+      "El nombre de sala que ingresaste ya existe. Intenta de nuevo con un nombre diferente"
+    );
+    const infoDoc = consulta.data();
+    console.log(infoDoc);
+    return;
+  } else {
+    // si no existe
+    const newRoom = setDoc(docRef, { players: [userEmail] });
+    if (newRoom) {
+      alert("Sala Creada!");
+      console.log(newRoom);
+    } else {
+      alert("Error al crear la sala");
+    }
+  }
+};
+
+const findRoom = async (userEmail, roomId) => {
+  // crear referencia
+  const docRef = doc(rooms, roomId);
+  // buscar doc
+  const consulta = await getDoc(docRef);
+  // revisar si existe
+  if (consulta.exists()) {
+    // si si existe
+    const newRoom = consulta.data();
+    const newRoomWithUser = await addUserToRoom(
+      docRef,
+      newRoom.players,
+      userEmail
+    );
+    return newRoomWithUser;
+  } else {
+    // si no existe
+    alert("La sala a la que tratas de unirte no existe");
+    return;
+  }
+};
+
+const addUserToRoom = async (docRef, players, userEmail) => {
+  console.log(players);
+  const room = { players: [...players, userEmail] };
+  await updateDoc(docRef, room);
+  const consulta = await getDoc(docRef);
+  if (consulta.exists()) {
+    const data = consulta.data();
+    console.log(data);
+    return data;
+  } else {
+    alert("No existe la sala a la que tratas de unirte.");
+  }
+};
+
+export default {
+  getAllBlackCards,
+  getAllWhiteCards,
+  getBlackCardById,
+  getWhiteCardById,
+  addBlackCard,
+  addWhiteCard,
+  addNewUser,
+  createRoom,
+  findRoom,
+};
+
 // old way updating to add new white card
 // export const addWhiteCardFS = async ({ form }) => {
 //     const docRef = doc(firestore, `cards/white-cards`);
@@ -112,13 +195,3 @@ export const addNewUser = async (user, roomId) => {
 //         console.log("No existe!!");
 //     }
 // };
-
-export default {
-  getAllBlackCards,
-  getAllWhiteCards,
-  getBlackCardById,
-  getWhiteCardById,
-  addBlackCard,
-  addWhiteCard,
-  addNewUser,
-};
