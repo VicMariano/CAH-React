209d3ff693dff8a7869193b1cd1111a85a48354c
VicMariano <marianovictoria.11@gmail.com>
2022-01-31 17:43:07 -0300

    Modify: Router

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,12 +1,20 @@
 import image from "../assets/CAH.png";
 
 import { getAuth } from "@firebase/auth";
+import { useHistory, Link } from "react-router-dom";
 import firebaseApp from "../services/firebaseCredentials";
 const auth = getAuth(firebaseApp);
 
 export default function Home() {
+  const history = useHistory();
+  const onRedirect = () => {
+    history.push("/login");
+  };
   return (
     <main style={{ padding: "1rem 0" }}>
+      <button onClick={onRedirect}>REDIRECT TO LOGIN</button>
+      <Link to="/collectionPage">REDIRECT TO COLLECTION</Link>
+
       <h1 style={{ padding: "1rem 0 2rem 0" }}>
         Cards Against Humanity ... mas o menos
       </h1>
